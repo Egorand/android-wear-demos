88a2221df4d6170fc72939fccc0a2da1c89402dc
Egor Andreevici <andreevich.egor@gmail.com>
2014-03-23 23:46:22 +0200

    - Work on simple notification

--- main/src/main/java/com/egorand/wear/main/MainActivity.java
+++ main/src/main/java/com/egorand/wear/main/MainActivity.java
@@ -2,6 +2,9 @@ package com.egorand.wear.main;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.preview.support.v4.app.NotificationManagerCompat;
+import android.support.v4.app.NotificationCompat;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void createSimpleNotification(View v) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentTitle("Android Wear")
+                .setContentText("Exploring the developer preview SDK");
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(1, builder.build());
     }
 }
